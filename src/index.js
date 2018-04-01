b2160631b4deb0e5b996656a6f0a7dde4bbdf212
Tirzah Morrison <tirzah@Tirzahs-MacBook-Air.local>
2018-04-01 14:18:45 -0400

    get categories

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
+const BASE_URL = "http://jservice.io/api"
+const values = [100, 200, 300, 400, 500]
 const app = angular.module("main", ["customFilters"]);
 
-app.controller("mainController", ($scope) => {
-    $scope.title = "Hello World!"
+app.controller("mainController", ["$scope", "$http", ($scope, $http) => {
+    
+    $http({
+        url: BASE_URL + "/random?count=5"
+      }).then(response => {
+        console.log(response)
+        console.log(response.data)
+        $scope.categories = response.data.map(c => c.category)
+      })
+      $scope.categories.forEach((category) => {
+    $http({
+        url: BASE_URL + `/clues?value=${}&category=${}`
+    })
 })
+      console.dir($scope)
+    }])
