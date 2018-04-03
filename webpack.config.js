7f79e8cb60050a059fbbed73f0ae4aa1d39526d3
Tirzah Morrison <tirzah@Tirzahs-MacBook-Air.local>
2018-04-02 22:47:36 -0400

    save

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
+const ExtractTextPlugin = require("extract-text-webpack-plugin"); // Require extract-text-webpack-plugin plugin
 const glob = require("glob");
 module.exports = {
   entry:glob.sync("./src/*.js"), // webpack entry point. Module to start building dependency graph
@@ -8,14 +9,24 @@ module.exports = {
     publicPath: '/' // public URL of the output directory when referenced in a browser
   },
   module: {  // where we defined file patterns and their loaders
-      rules: [ 
+      rules: [  
+           { 
+        test: /\.html$/,
+        exclude: __dirname + "/public/index.html",
+        loader: 'file-loader',
+        options: {
+            name: '[name].[ext]',
+            outputPath: 'templates/'
+        }
+    },
       ]
   },
   plugins: [  // Array of plugins to apply to build chunk
       new HtmlWebpackPlugin({
           template: __dirname + "/public/index.html",
           inject: 'body'
-      })
+      }),
+      new ExtractTextPlugin('bundle.css')
   ],
   devServer: {  // configuration for webpack-dev-server
       contentBase: './public',  //source of static assets
